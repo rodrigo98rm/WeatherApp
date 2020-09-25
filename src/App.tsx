b836f5a7f82094198d7eeecedf3caefffe4f3b14
Rodrigo Mayer <mayerrodrigo98@gmail.com>
2020-09-24 22:46:08 -0300

    Eslint, prettier, editorconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,9 @@
 import React from 'react';
 
-interface Weather {
-  info: {
-    tempMax: number;
-    tempMin: number;
-    type: 'Cloudy' | 'Clear';
-    isEndOfWorld?: boolean;
-  };
-}
-
-const Weather: React.FC<Weather> = ({ info }) => {
-  return (
-    <div>
-      <p>{info.type}</p>
-    </div>
-  );
-};
+import Overview from './components/Overview';
 
 const App: React.FC = () => {
-  return (
-    <Weather
-      info={{
-        tempMax: 39.4,
-        tempMin: 32.6,
-        type: 'Cloudy',
-        isEndOfWorld: true,
-      }}
-    />
-  );
+	return <Overview />;
 };
 
 export default App;
---
+++ src/components/Overview/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Overview: React.FC = () => {
+	return (
+		<Container>
+			<p>Hello World!</p>
+		</Container>
+	);
+};
+
+export default Overview;
